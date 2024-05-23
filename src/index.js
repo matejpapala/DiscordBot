9dcad8bc8677bd0506ad5c3bb0794879571321a4
Matěj Papala <matejpapala1234@gmail.com>
2024-05-24 00:46:52 +0200

    fix commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const { Client, IntentsBitField } = require("discord.js");
+require("dotenv").config();
+
+let usernameForDeletion = "Bongo";
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+client.on("ready", (c) => {
+  console.log(`${c.user.username} is running`);
+});
+
+client.on("messageCreate", (sentMessage) => {
+  console.log(sentMessage.content);
+  if (sentMessage.author.username === usernameForDeletion) {
+    setTimeout(() => {
+      sentMessage.delete();
+    }, 5000);
+  } else if (sentMessage.author.username === "matikorba") {
+    sentMessage.reply("jo dik");
+  }
+});
+
+client.on("interactionCreate", (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+  if (interaction.commandName === "hey") {
+    interaction.reply("hey!");
+  }
+});
+
+client.login(process.env.TOKEN);
